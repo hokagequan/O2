4e24603ac1768c7a06452c443959af34fb99e074
hokagequan <hokagequan@gmail.com>
2015-09-22 21:56:07 +0800

    网络请求

--- Code/Trunk/O2Trip /O2Trip/NewO2Trip/Managers/HttpReqManager.swift
+++ Code/Trunk/O2Trip /O2Trip/NewO2Trip/Managers/HttpReqManager.swift
@@ -10,4 +10,39 @@ import Foundation
 
 class HttpReqManager {
     
+    static let httpUrl = "http://test.o2lx.com:9090/trip/ws/"
+    static let imageUrl = "http://test.o2lx.com:9090/trip"
+    
+    // 获取购物车信息
+    class func httpRequestShoppingCart(userID: String, start: String, count: String, completion: ((Dictionary<String, AnyObject>) -> Void)?, failure: ((ErrorType) -> Void)?) {
+        var params = Dictionary<String, String>()
+        params["userId"] = userID
+        params["startPage"] = start
+        params["pageSize"] = count
+        self.httpRequest("rest_shopping/getShoppingCar", params: params, completion: { (response) -> Void in
+            completion?(response)
+            }) { (error) -> Void in
+                failure?(error)
+        }
+    }
+    
+    // MARK: - Private
+    
+    private class func httpRequest(method: String, params: Dictionary<String, String>, completion: ((Dictionary<String, AnyObject>) -> Void)?, failure: ((ErrorType) -> Void)?) {
+        do {
+            let jsonData = try NSJSONSerialization.dataWithJSONObject(params, options: NSJSONWritingOptions.PrettyPrinted)
+            let jsonParams = String(data: jsonData, encoding: NSUTF8StringEncoding)
+            let url = "\(httpUrl)\(method)?paramjson={\(jsonParams)}"
+            
+            let afManager = AFHTTPRequestOperationManager()
+            afManager.GET(url, parameters: "", success: { (operation, response) -> Void in
+                completion?(response as! Dictionary<String, AnyObject>)
+                }, failure: { (operation, error) -> Void in
+                    failure?(error)
+            })
+        }
+        catch let error {
+            failure?(error)
+        }
+    }
 }
