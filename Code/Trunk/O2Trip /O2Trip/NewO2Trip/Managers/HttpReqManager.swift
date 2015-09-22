2b17d3f32474c441242369bf262eca8f5d24ff8a
hokagequan <hokagequan@gmail.com>
2015-09-22 17:32:11 +0800

    购物车

---
+++ Code/Trunk/O2Trip /O2Trip/NewO2Trip/Managers/HttpReqManager.swift
@@ -0,0 +1,13 @@
+//
+//  HttpReqManager.swift
+//  O2Trip
+//
+//  Created by Q on 15/9/22.
+//  Copyright © 2015年 lst. All rights reserved.
+//
+
+import Foundation
+
+class HttpReqManager {
+    
+}
